ea39f4e2e0f97fafc05835c0c2d7b3fab1508c8e
Will McMain <willmcmain@gmailc.om>
2017-10-08 23:53:39 -0400

    Fix broken neovim install

--- neovim/install.sh
+++ neovim/install.sh
@@ -7,8 +7,10 @@ source $DIR/../functions.sh
 
 if [ "$(uname -s)" == "Darwin" ]; then
     brew install neovim
-elif [ "$(uname -s)" == "Linux"]; then
+elif [ "$(uname -s)" == "Linux" ]; then
     sudo apt install neovim
 fi
 
 link_file ~/.vimrc $DIR/vimrc
+mkdir -p ~/.config/nvim/
+link_file ~/.config/nvim/init.vim $DIR/vimrc
