0be511e4474ff050ebfa9345bb3020e07c1451b9
Will McMain <willmcmain@gmail.com>
2017-10-08 22:43:42 -0400

    Add bash prompt with git stuff

---
+++ bash/bashrc.d/bash-colors.sh
@@ -0,0 +1,78 @@
+# Color definitions for bash colors
+# Primarily used in bash prompt (.bashrc), but could be used anywhere.
+#
+# Usage: Before defining PS1, run:
+#   source .bash-colors
+
+# Reset
+export Color_NoColor='\[\e[m\]'       # Text Reset
+
+# Regular Colors
+export Color_Black='\[\e[0;30m\]'        # Black
+export Color_Red='\[\e[0;31m\]'          # Red
+export Color_Green='\[\e[0;32m\]'        # Green
+export Color_Yellow='\[\e[0;33m\]'       # Yellow
+export Color_Blue='\[\e[0;34m\]'         # Blue
+export Color_Purple='\[\e[0;35m\]'       # Purple
+export Color_Cyan='\[\e[0;36m\]'         # Cyan
+export Color_White='\[\e[0;37m\]'        # White
+
+# Bold
+export Color_BBlack='\[\e[1;30m\]'       # Black
+export Color_BRed='\[\e[1;31m\]'         # Red
+export Color_BGreen='\[\e[1;32m\]'       # Green
+export Color_BYellow='\[\e[1;33m\]'      # Yellow
+export Color_BBlue='\[\e[1;34m\]'        # Blue
+export Color_BPurple='\[\e[1;35m\]'      # Purple
+export Color_BCyan='\[\e[1;36m\]'        # Cyan
+export Color_BWhite='\[\e[1;37m\]'       # White
+
+# Underline
+export Color_UBlack='\[\e[4;30m\]'       # Black
+export Color_URed='\[\e[4;31m\]'         # Red
+export Color_UGreen='\[\e[4;32m\]'       # Green
+export Color_UYellow='\[\e[4;33m\]'      # Yellow
+export Color_UBlue='\[\e[4;34m\]'        # Blue
+export Color_UPurple='\[\e[4;35m\]'      # Purple
+export Color_UCyan='\[\e[4;36m\]'        # Cyan
+export Color_UWhite='\[\e[4;37m\]'       # White
+
+# Background
+export Color_On_Black='\[\e[40m\]'       # Black
+export Color_On_Red='\[\e[41m\]'         # Red
+export Color_On_Green='\[\e[42m\]'       # Green
+export Color_On_Yellow='\[\e[43m\]'      # Yellow
+export Color_On_Blue='\[\e[44m\]'        # Blue
+export Color_On_Purple='\[\e[45m\]'      # Purple
+export Color_On_Cyan='\[\e[46m\]'        # Cyan
+export Color_On_White='\[\e[47m\]'       # White
+
+# High Intensity
+export Color_IBlack='\[\e[0;90m\]'       # Black
+export Color_IRed='\[\e[0;91m\]'         # Red
+export Color_IGreen='\[\e[0;92m\]'       # Green
+export Color_IYellow='\[\e[0;93m\]'      # Yellow
+export Color_IBlue='\[\e[0;94m\]'        # Blue
+export Color_IPurple='\[\e[0;95m\]'      # Purple
+export Color_ICyan='\[\e[0;96m\]'        # Cyan
+export Color_IWhite='\[\e[0;97m\]'       # White
+
+# Bold High Intensity
+export Color_BIBlack='\[\e[1;90m\]'      # Black
+export Color_BIRed='\[\e[1;91m\]'        # Red
+export Color_BIGreen='\[\e[1;92m\]'      # Green
+export Color_BIYellow='\[\e[1;93m\]'     # Yellow
+export Color_BIBlue='\[\e[1;94m\]'       # Blue
+export Color_BIPurple='\[\e[1;95m\]'     # Purple
+export Color_BICyan='\[\e[1;96m\]'       # Cyan
+export Color_BIWhite='\[\e[1;97m\]'      # White
+
+# High Intensity backgrounds
+export Color_On_IBlack='\[\e[0;100m\]'   # Black
+export Color_On_IRed='\[\e[0;101m\]'     # Red
+export Color_On_IGreen='\[\e[0;102m\]'   # Green
+export Color_On_IYellow='\[\e[0;103m\]'  # Yellow
+export Color_On_IBlue='\[\e[0;104m\]'    # Blue
+export Color_On_IPurple='\[\e[0;105m\]'  # Purple
+export Color_On_ICyan='\[\e[0;106m\]'    # Cyan
+export Color_On_IWhite='\[\e[0;107m\]'   # White
--- bash/bashrc.d/prompt.sh
+++ bash/bashrc.d/prompt.sh
@@ -1,11 +1,109 @@
 #! /bin/bash
 
-PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+export SUDO_PS1="${Color_IWhite}${Color_On_Red}\u@\h: \W#${Color_NoColor} "
+
+# TODO: Rewrite this to show the last two directories instead of shortening by length
+shorten_path () {
+  #arg: path to shorten (defaults to $PWD)
+  path="${1:-$PWD}"
+  shortened=$(echo "${path/#$HOME/\~}" | awk -F "/" '
+    {if (length($0) > 35)
+      {if (NF>4)
+        print $1 "/" $2 "/.../" $(NF-1) "/" $NF;
+      else if (NF>3) 
+        print $1 "/" $2 "/.../" $NF; 
+      else 
+        print $1 "/.../" $NF; 
+    } else 
+      print $0;}
+  ')
+  echo "${shortened}"
+}
+
+
+standard_prompt () {
+  echo "[${Color_IGreen}\u${Color_NoColor}@${Color_IWhite}${Color_On_Red}\h${Color_NoColor}] ${Color_Cyan}\w${Color_NoColor}"
+}
+
+
+git_prompt () {
+  local git_branch r s symbol
+
+  git_branch=$(git symbolic-ref HEAD 2>&1)
+  r=$?
+  if [ "$git_branch" = "fatal: ref HEAD is not a symbolic ref" ]; then 
+    echo "${Color_Red}???${Color_NoColor} "
+  
+  elif [ $r -eq 0 ]; then
+    git_branch="${git_branch#refs/heads/}"
+    s=`git status`
+    
+    if grep -q "ahead of" <<<"$s"; then
+      symbol="+"
+    elif grep -q "behind" <<<"$s"; then
+      symbol="-"
+    elif grep -q "diverged" <<<"$s"; then
+      symbol='!!'
+    fi
+
+    if grep -q "nothing to commit" <<<"$s"; then
+      git_branch="(${Color_Green}$git_branch${Color_NoColor})"
+      index_symbol=""
+    elif grep -q -E "Untracked files|not staged" <<<"$s"; then
+      git_branch="(${Color_Yellow}$git_branch${Color_NoColor})"
+      index_symbol="?"
+    elif grep -q "Changes to be committed" <<<"$s"; then
+      git_branch="(${Color_Red}$git_branch${Color_NoColor})"
+      index_symbol="!"
+    fi
+    
+    [ -n "$git_branch" ] && echo "${Color_BYellow}${symbol}${index_symbol}${Color_NoColor}${git_branch}"
+  fi
+}
+
+
+git_branch () {
+  toplevel=$(git rev-parse --show-toplevel)
+  echo "${Color_Purple}"$(basename "$toplevel")"${Color_NoColor}"
+}
+
+
+git_path () {
+  toplevel="$(git rev-parse --show-toplevel)"
+  shortened=-$([ "$toplevel" != "$PWD" ] && shorten_path "${PWD#$toplevel}")
+  echo "${Color_Cyan}${shortened}${Color_NoColor}"
+}
+
+
+set_prompt () {
+  gitstuff=$(git_prompt)
+  if [ -n "$VIRTUAL_ENV" ]; then
+    venv="${Color_Yellow}$(basename $VIRTUAL_ENV)${Color_NoColor} "
+  else
+    venv=""
+  fi
+
+  if [ -n "$gitstuff" ]; then
+    prompt="$(git_branch) $(git_prompt): $(git_path)"
+  else
+    prompt="$(standard_prompt)"
+  fi
+  export PS1="${venv}${prompt} \$ "
+}
+
+
+if [ -n "$PROMPT_COMMAND" ]; then
+  PROMPT_COMMAND="$PROMPT_COMMAND; set_prompt"
+else
+  PROMPT_COMMAND="set_prompt"
+fi
+
+export PROMPT_DIRTRIM=2
 
 # Set title if using xterm
 case "$TERM" in
 xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    PS1="\[\e]0;\u@\h: \w\a\]$PS1"
     ;;
 *)
     ;;
