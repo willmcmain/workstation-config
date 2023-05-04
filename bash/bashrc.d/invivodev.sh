adb6b520557599da8fb7d479abc8ae4edf729461
Will McMain <willmcmain@gmail.com>
2023-05-04 11:50:10 -0400

    Add invivodev env vars

---
+++ bash/bashrc.d/invivodev.sh
@@ -0,0 +1,6 @@
+export WORKSTATION_CONFIG=ivodev-010
+export TUNNEL_LOCAL_PORT=2222
+export PROJECT=viumworks-develo-1605835844206
+export CLUSTER=ivodev-cluster
+export REGION=us-west1
+export WORKSTATION_NAME=willmcmain-workstation-001
--- bash/bashrc.d/vscode.sh
+++ bash/bashrc.d/vscode.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
